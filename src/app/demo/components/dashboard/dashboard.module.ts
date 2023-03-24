e3f60cf7ea4f4c1c217c0fc3924ac8144b0ffe36
Tien <phamlenhattien1997@gmail.com>
2023-03-24 16:10:23 +0700

    define dashboard component

--- src/app/demo/components/dashboard/dashboard.module.ts
+++ src/app/demo/components/dashboard/dashboard.module.ts
@@ -22,6 +22,7 @@ import { TreemapChartComponent } from './sale-by-channel/treemap-chart/treemap-c
 import { SaleByChannelHeatmapComponent } from './total-sale-by-channel/sale-by-channel-heatmap/sale-by-channel-heatmap.component';
 import { LinechartComponent } from './totalsalechart/linechart/linechart.component';
 import { TotalsalechartComponent } from './totalsalechart/totalsalechart.component';
+import { PaginatorModule } from 'primeng/paginator';
 @NgModule({
   imports: [
     CommonModule,
@@ -35,6 +36,7 @@ import { TotalsalechartComponent } from './totalsalechart/totalsalechart.compone
     DashboardsRoutingModule,
     CalendarModule,
     NgApexchartsModule,
+    PaginatorModule,
   ],
   declarations: [
     DashboardComponent,
