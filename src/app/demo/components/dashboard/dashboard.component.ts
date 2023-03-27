eeff6d1011cd4b73905cee70143e68b55e22dd32
Tien <phamlenhattien1997@gmail.com>
2023-03-27 11:23:38 +0700

    define dashboard-statistic-detail

--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -4,100 +4,184 @@ import { Product } from '../../api/product';
 import { ProductService } from '../../service/product.service';
 import { Subscription } from 'rxjs';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { DetailStatistic } from './dashboard-statistic/detail-statistic/detail-statistic.component';
 
 @Component({
-    templateUrl: './dashboard.component.html',
+  templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit, OnDestroy {
+  statisticData: DetailStatistic[] = [
+    {
+      title: 'order',
+      nameManage: 'Orders',
+      manageUrl: './',
+      data: [
+        {
+          displayText: 'New Orders',
+          value: 234,
+        },
+        {
+          displayText: 'Issue',
+          value: 2,
+          hasCircle: true,
+          circleColor: 'red',
+        },
+      ],
+    },
+    {
+      title: 'product inventory',
+      nameManage: 'product',
+      manageUrl: './',
+      data: [
+        {
+          displayText: '#Products Out of Stock',
+          value: 12,
+          hasCircle: true,
+          circleColor: 'red',
+        },
+        {
+          displayText: '#Number Products Nearly out of Stock',
+          value: 2,
+          hasCircle: true,
+          circleColor: 'red',
+        },
+      ],
+      hasFooter: true,
+      footer: {
+        displayText: 'Not Selling Products in last 3 Months',
+        value: 345,
+        url: './',
+      },
+    },
+    {
+      title: 'product',
+      nameManage: 'product',
+      manageUrl: './',
+      data: [
+        {
+          displayText: 'Inactive',
+          value: 7,
+          hasCircle: true,
+          circleColor: 'red',
+        },
+      ],
+    },
+    {
+      title: 'channel',
+      nameManage: 'channel',
+      manageUrl: './',
+      data: [
+        {
+          displayText: 'Active Channel',
+          value: 9,
+          hasCircle: true,
+          circleColor: 'green',
+        },
+        {
+          displayText: 'Inactive Channel',
+          value: 1,
+          hasCircle: true,
+          circleColor: 'red',
+        },
+      ],
+    },
+  ];
 
-    items!: MenuItem[];
+  items!: MenuItem[];
 
-    products!: Product[];
+  products!: Product[];
 
-    chartData: any;
+  chartData: any;
 
-    chartOptions: any;
+  chartOptions: any;
 
-    subscription!: Subscription;
+  subscription!: Subscription;
 
-    constructor(private productService: ProductService, public layoutService: LayoutService) {
-        this.subscription = this.layoutService.configUpdate$.subscribe(() => {
-            this.initChart();
-        });
+  constructor(
+    private productService: ProductService,
+    public layoutService: LayoutService
+  ) {
+    this.subscription = this.layoutService.configUpdate$.subscribe(() => {
+      this.initChart();
+    });
+  }
+
+  ngOnInit() {
+    this.initChart();
+    this.productService
+      .getProductsSmall()
+      .then((data) => (this.products = data));
+
+    this.items = [
+      { label: 'Add New', icon: 'pi pi-fw pi-plus' },
+      { label: 'Remove', icon: 'pi pi-fw pi-minus' },
+    ];
+  }
+
+  initChart() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue(
+      '--text-color-secondary'
+    );
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.chartData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          backgroundColor: documentStyle.getPropertyValue('--bluegray-700'),
+          borderColor: documentStyle.getPropertyValue('--bluegray-700'),
+          tension: 0.4,
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          backgroundColor: documentStyle.getPropertyValue('--green-600'),
+          borderColor: documentStyle.getPropertyValue('--green-600'),
+          tension: 0.4,
+        },
+      ],
+    };
+
+    this.chartOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
-
-    ngOnInit() {
-        this.initChart();
-        this.productService.getProductsSmall().then(data => this.products = data);
-
-        this.items = [
-            { label: 'Add New', icon: 'pi pi-fw pi-plus' },
-            { label: 'Remove', icon: 'pi pi-fw pi-minus' }
-        ];
-    }
-
-    initChart() {
-        const documentStyle = getComputedStyle(document.documentElement);
-        const textColor = documentStyle.getPropertyValue('--text-color');
-        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-        this.chartData = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--bluegray-700'),
-                    borderColor: documentStyle.getPropertyValue('--bluegray-700'),
-                    tension: .4
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40, 19, 86, 27, 90],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--green-600'),
-                    borderColor: documentStyle.getPropertyValue('--green-600'),
-                    tension: .4
-                }
-            ]
-        };
-
-        this.chartOptions = {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: textColor
-                    }
-                }
-            },
-            scales: {
-                x: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
-                }
-            }
-        };
-    }
-
-    ngOnDestroy() {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-        }
-    }
+  }
 }
