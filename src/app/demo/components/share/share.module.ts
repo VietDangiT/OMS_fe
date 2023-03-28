04c4f8e7a3ab9fb66e7334d6bbb85fabab01d404
Trung Tin <haphamtrungtin@gmail.com>
2023-03-28 17:02:10 +0700

    update date filter component

--- src/app/demo/components/share/share.module.ts
+++ src/app/demo/components/share/share.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 import { OMSChartComponent } from './oms-chart/oms-chart.component';
 import { ChartModule } from 'primeng/chart';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { DateFilterComponent } from './date-filter/date-filter.component';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    OMSChartComponent
+    OMSChartComponent,
+    DateFilterComponent
   ],
   imports: [
     CommonModule,
     ChartModule,
     NgApexchartsModule,
+    CalendarModule,
+    FormsModule
   ],
-  exports: [OMSChartComponent]
+  exports: [OMSChartComponent, DateFilterComponent]
 })
 export class ShareModule { }
