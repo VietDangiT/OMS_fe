c9d1f56a15d412db98e5d638268e519d0222a07e
Tuan Duong Manh <dmtuan1995@gmail.com>
2023-03-29 22:28:55 +0700

    remove uneccessary import

--- src/app/demo/service/dashboard.service.ts
+++ src/app/demo/service/dashboard.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Order } from '../api/order';
 
 @Injectable({
   providedIn: 'root',
