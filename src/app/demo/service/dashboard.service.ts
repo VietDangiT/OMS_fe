153ccac333235d353506259e01df3842a7773531
Tuan Duong Manh <dmtuan1995@gmail.com>
2023-03-29 22:14:51 +0700

    fix commit

--- src/app/demo/service/dashboard.service.ts
+++ src/app/demo/service/dashboard.service.ts
@@ -8,51 +8,65 @@ import { Order } from '../api/order';
   providedIn: 'root',
 })
 export class DashboardService {
-
-
-
   saleByLocationURL!: string;
   constructor(private _http: HttpClient) {
     this.saleByLocationURL = environment.baseURL + '/sale-by-location';
-    
   }
 
-  getSaleOnChannel(url: string){
+  getSaleOnChannel(url: string) {
     return this._http.get(url).pipe();
   }
 
   getSaleByLocation(location: string): Observable<any> {
     var country = location.toLowerCase().split(' ').join();
-  
-    return this._http.get(`${this.saleByLocationURL}/sale-${country}.json`).pipe();
+
+    return this._http
+      .get(`${this.saleByLocationURL}/sale-${country}.json`)
+      .pipe();
   }
 
-  getCountriesSaleInTotal(date: any): Observable<any>{
-    if(date != null){
-      return this._http.get(`${this.saleByLocationURL}/total-countries-sale-by-${date}.json`).pipe();
+  getCountriesSaleInTotal(date: any): Observable<any> {
+    if (date != null) {
+      return this._http
+        .get(`${this.saleByLocationURL}/total-countries-sale-by-${date}.json`)
+        .pipe();
     }
-    return this._http.get(`${this.saleByLocationURL}/total-countries-sale-by-week.json`).pipe();
+    return this._http
+      .get(`${this.saleByLocationURL}/total-countries-sale-by-week.json`)
+      .pipe();
   }
 
-  getLeads(date: any): Observable<any>{
-    if(date != null){
-      return this._http.get(`${this.saleByLocationURL}/leads-by-${date}.json`).pipe();
+  getLeads(date: any): Observable<any> {
+    if (date != null) {
+      return this._http
+        .get(`${this.saleByLocationURL}/leads-by-${date}.json`)
+        .pipe();
     }
-    return this._http.get(`${this.saleByLocationURL}/leads-by-week.json`).pipe();
+    return this._http
+      .get(`${this.saleByLocationURL}/leads-by-week.json`)
+      .pipe();
   }
 
-  getCountriesSale(date: any): Observable<any>{
-    if(date != null){
-      return this._http.get(`${this.saleByLocationURL}/countries-sale-by-${date}.json`).pipe();
+  getCountriesSale(date: any): Observable<any> {
+    if (date != null) {
+      return this._http
+        .get(`${this.saleByLocationURL}/countries-sale-by-${date}.json`)
+        .pipe();
     }
-    return this._http.get(`${this.saleByLocationURL}/countries-sale-by-week.json`).pipe();
+    return this._http
+      .get(`${this.saleByLocationURL}/countries-sale-by-week.json`)
+      .pipe();
   }
 
-  getTableData(pageNumber: number = 0){
-    if(pageNumber < 0){
-      return this._http.get(`${this.saleByLocationURL}/table-data-0.json`).pipe();
+  getTableData(pageNumber: number = 0) {
+    if (pageNumber < 0) {
+      return this._http
+        .get(`${this.saleByLocationURL}/table-data-0.json`)
+        .pipe();
     }
-    return this._http.get(`${this.saleByLocationURL}/table-data-${pageNumber}.json`).pipe();
+    return this._http
+      .get(`${this.saleByLocationURL}/table-data-${pageNumber}.json`)
+      .pipe();
   }
 
   GetOrders() {
