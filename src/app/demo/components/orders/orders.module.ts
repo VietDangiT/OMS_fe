e0745a275b2c4c9d14c3d46df0f7b6c7bc0a8202
Tin Ha <haphamtrungtin@gmail.com>
2023-05-19 11:19:15 +0700

    chore: update checkbox for table

---
+++ src/app/demo/components/orders/orders.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrdersRoutingModule } from './orders-routing.module';
+import { OrdersComponent } from './orders.component';
+import { OrderListComponent } from './order-list/order-list.component';
+
+
+@NgModule({
+  declarations: [
+    OrdersComponent,
+    OrderListComponent
+  ],
+  imports: [
+    CommonModule,
+    OrdersRoutingModule
+  ]
+})
+export class OrdersModule { }
